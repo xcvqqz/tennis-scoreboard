7aec49fbaef2b0ff590525aa71d2e7e1e3a9e22f
Максим <Grossholder77@gmail.com>
2025-10-19 00:23:48 +0300

    feat: создание BasicController -  родителя всех Controller

--- src/main/Java/controller/BasicController.java
+++ src/main/Java/controller/BasicController.java
@@ -6,9 +6,7 @@ import service.OngoingMatchesService;
 import util.UUIDUtil;
 
 public class BasicController extends HttpServlet {
-
     protected final OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
     protected final UUIDUtil uuidUtil = new UUIDUtil();
     protected final FinishedMatchesPersistenceService finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
-
 }
