4a8a104a3be19cfe1a02a5c684652e95100acf6d
Максим <Grossholder77@gmail.com>
2025-10-25 15:00:31 +0300

    ref: 1) рефакторинг BasicController

--- src/main/Java/controller/BasicController.java
+++ src/main/Java/controller/BasicController.java
@@ -8,16 +8,10 @@ import util.UUIDUtil;
 
 public class BasicController extends HttpServlet {
 
-
-
-
     protected final OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
     protected final UUIDUtil uuidUtil = new UUIDUtil();
     protected final FinishedMatchesPersistenceService finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
     protected final PlayerService playerService = new PlayerService();
-    protected final MatchService matchService = new MatchService();
-
-
 
 
 }
