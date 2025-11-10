dd25f54f9cd119549eb263fdd3da0e7d4438864c
Максим <Grossholder77@gmail.com>
2025-11-10 15:35:55 +0300

    feat: создание метода shouldСontinueGameAfterFortyFortyGamePoints() в MatchScoreCalculationServiceTest

--- src/test/java/MatchScoreCalculationServiceTest.java
+++ src/test/java/MatchScoreCalculationServiceTest.java
@@ -1,15 +1,27 @@
 
 
+import io.github.xcvqqz.tennis_scoreboard.dto.MatchDTO;
+import io.github.xcvqqz.tennis_scoreboard.dto.PlayerDTO;
+import io.github.xcvqqz.tennis_scoreboard.service.MatchScoreCalculationService;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
 
 public class MatchScoreCalculationServiceTest {
 
+
+    private final MatchDTO matchDTOTest = createNewTestMatch();
+    private final MatchScoreCalculationService matchScoreCalculationServiceTest = new MatchScoreCalculationService(matchDTOTest);
+
+
+    //1) Если игрок 1 выигрывает очко при счёте 40-40, гейм не заканчивается
     @Test
-    public void shouldСontinueGameOnAdvantageAfterDeuce(){
+    public void shouldСontinueGameAfterFortyFortyGamePoints(){
+        assertFalse(isGameOverAfterFortyForty(matchDTOTest));
     }
 
-
-
+    //2) Если игрок 1 выигрывает очко при счёте 40-0, то он выигрывает и гейм
     @Test
     public void shouldWinGameWhenScoringAtFortyZero(){
 
@@ -20,7 +32,32 @@ public class MatchScoreCalculationServiceTest {
 
     }
 
+
+
+    private MatchDTO createNewTestMatch(){
+        PlayerDTO playerOneTest = new PlayerDTO();
+        PlayerDTO playerTwoTest = new PlayerDTO();
+        return new MatchDTO(playerOneTest,playerTwoTest);
+    }
+
+    private void setFortyScoreForPlayer(PlayerDTO playerDTOTets){
+        playerDTOTets.getMatchScoreDTO().setMatchScore(40);
+    }
+
+    private boolean isGameOverAfterFortyForty(MatchDTO matchDTO){
+        setFortyScoreForPlayer(matchDTOTest.getPlayerOne());
+        setFortyScoreForPlayer(matchDTOTest.getPlayerTwo());
+        matchScoreCalculationServiceTest.addPoint(matchDTO.getPlayerOne());
+        if(matchDTO.getPlayerOne().getMatchScoreDTO().getMatchScore() == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
+
 
 
 
@@ -33,5 +70,5 @@ public class MatchScoreCalculationServiceTest {
 //Покроем юнит тестами подсчёт очков в матче. Примеры кейсов:
 //
 //1) Если игрок 1 выигрывает очко при счёте 40-40, гейм не заканчивается
-//2)Если игрок 1 выигрывает очко при счёте 40-0, то он выигрывает и гейм
+//
 //3) При счёте 6-6 начинается тайбрейк вместо обычного гейма
